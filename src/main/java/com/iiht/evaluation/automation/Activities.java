08f00ea9455214bcb9d61d3c6ca8f0aa7df92820
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-19 18:56:08 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/evaluation/automation/Activities.java
@@ -0,0 +1,56 @@
+package com.iiht.evaluation.automation;
+
+import org.openqa.selenium.WebDriver;
+
+public class Activities {
+    public static boolean click_shop_menu(WebDriver driver) {
+        return false;
+    }
+    public static boolean click_home_menu_button(WebDriver driver) {
+        return false;
+    }
+    public static boolean check_home_page_has_three_arrivals(WebDriver driver) {
+        return false;
+    }
+    public static boolean click_product_product_image_in_arrival(WebDriver driver, String product_name) {
+        return false;
+    }
+    public static int get_number_of_item_in_cart(WebDriver driver){
+        return -1;
+    }
+    public static boolean click_add_to_basket_button_product_detail_page(WebDriver driver) {
+        return false;
+    }
+    public static boolean check_product_added_message_product_detail_page(WebDriver driver, String product_name) {
+        return false;
+    }
+
+    public static boolean click_item_detail_menu(WebDriver driver){
+        return false;
+    }
+
+    
+
+    public static boolean check_product_total_checkout_page(WebDriver driver, String product_name){
+        return false;
+    }
+
+    public static boolean check_basket_total_checkout_page(WebDriver driver, String product_name){
+        return false;
+    }
+
+    public static boolean remove_product_from_checkout_page(WebDriver driver, String product_name){
+        return false;
+    }
+
+    public static boolean update_basket_product_from_checkout_page(WebDriver driver, String product_name, String item_quantity) throws InterruptedException {
+        return false;
+    }
+
+    public static boolean click_proceed_to_checkout_from_checkout_page(WebDriver driver) throws InterruptedException {
+        return false;
+    }
+
+}
+
+
